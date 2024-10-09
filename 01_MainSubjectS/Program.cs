53fbef6f97bd9d94b47e6f3a8c6606fc7ad5c7f6
Hüsamettin demir <husamettin1498@gmail.com>
2024-10-09 15:27:12 +0300

    int değişkenler tamamlandı

--- 01_MainSubjectS/Program.cs
+++ 01_MainSubjectS/Program.cs
@@ -13,27 +13,55 @@ namespace _01_MainSubjectS
             #region Yazdırma komutları
             //Console.WriteLine("HELOO WORLD");
 
-            Console.WriteLine("*****Yemek Kategorileri************");
-            Console.WriteLine("");
-            Console.WriteLine("1-Çorbalar");
-            Console.WriteLine("2-Ana Yemekler");
-            Console.WriteLine("3-Soğuk başlangıçlar");
-            Console.WriteLine("4-Salatalar");
-            Console.WriteLine("5-tatlılar");
-            Console.WriteLine("6-İçecekler");
-            Console.WriteLine("---------------------------------------");
+            //Console.WriteLine("*****Yemek Kategorileri************");
+            //Console.WriteLine("");
+            //Console.WriteLine("1-Çorbalar");
+            //Console.WriteLine("2-Ana Yemekler");
+            //Console.WriteLine("3-Soğuk başlangıçlar");
+            //Console.WriteLine("4-Salatalar");
+            //Console.WriteLine("5-tatlılar");
+            //Console.WriteLine("6-İçecekler");
+            //Console.WriteLine("---------------------------------------");
 
             #endregion
 
-            #region Değişkneller
-            string name;
-            name = "dfks";
-            Console.WriteLine(name);
-            string customerName, district, city;
+            #region StringDeğişkneller
+            //string name;
+            //name = "dfks";
+            //Console.WriteLine(name);
+            //string customerName, district, city;
 
-            customerName = "HUam";
-            Console.WriteLine("********Rezervasyon***********")
-            Console.WriteLine("Müşteri:"+customerName);
+            //customerName = "HUam";
+            //Console.WriteLine("********Rezervasyon***********");
+            //Console.WriteLine("Müşteri:"+customerName);
+            #endregion
+
+            #region IntDeğişkenler
+
+            int number = 24;
+
+            int hamburgerPrice = 330;
+            int cokePrice = 35;
+            int pizzaPrice = 450;
+            int waterPrice = 10;
+            Console.WriteLine("------------Restoran menusu-----------------");
+            Console.WriteLine($"-----Hamburger: {hamburgerPrice}");
+            Console.WriteLine($"-----Coke: {cokePrice}");
+            Console.WriteLine($"-----Pizza: {pizzaPrice}");
+            Console.WriteLine($"-----Water: {waterPrice}");
+
+            int hamburgerCount = 3;
+            int cokeCount = 3;
+
+            int totalHamburger = hamburgerCount * hamburgerPrice;
+            int totalCoke = cokePrice * cokeCount;
+            Console.WriteLine("Tutarlar:/");
+            Console.WriteLine($"-{hamburgerCount} hamburger toplam fiyatı: {totalHamburger}");
+            Console.WriteLine($"-{cokeCount} kola toplam fiyatı: {totalCoke}");
+
+
+
+
             #endregion
 
             Console.Read();
